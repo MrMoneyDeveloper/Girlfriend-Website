0c5d0a26de5380cab9c0d3a1af7d8907342d3d77
MohammedFarhaanBuckas <MohammedFarhaanBuckas@outlook.com>
2025-07-08 11:59:24 +0200

    root

--- my-lenis-anime/vite.config.js
+++ my-lenis-anime/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  /** 👇 MUST match repo name for GitHub Pages */
+  base: '/my-lenis-anime/',
 })
